c896a732f3a6bd4d2e63f4ac626e04b5373904bc
Richard Ramírez <rramirez34@yahoo.com>
2025-09-18 23:05:23 -0500

    ss8

--- semana08/dia01-01-js-hangman-game/main.js
+++ semana08/dia01-01-js-hangman-game/main.js
@@ -5,20 +5,59 @@ const botonReiniciar = document.querySelector('.ahorcado__reiniciar')
 
 // palabraOculta.textContent = 'HOLA A TODOS'
 
+let letrasAdivinadas = ''
+
 const PALABRA_RESPUESTA = 'JAVASCRIPT'
 
 const ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('') // Un arreglo
 
 const renderizarAlfabeto = () => {
-  let contenido = ''
-
   ALFABETO.forEach(letra => {
-    contenido = contenido + `
-      <button class="bg-blue-500 p-2 text-white font-bold text-xl cursor-pointer hover:bg-blue-600 duration-300 rounded">${letra}</button>
-    `
+    const boton = document.createElement('button')
+    
+    boton.textContent = letra
+
+    boton.className = 'bg-blue-500 p-2 text-white font-bold text-xl cursor-pointer hover:bg-blue-600 duration-300 rounded'
+
+    boton.addEventListener('click', () => {
+      letrasAdivinadas += letra
+
+      console.log('letrasAdivinadas', letrasAdivinadas)
+
+      palabraOculta.textContent = PALABRA_RESPUESTA
+        .split('')
+        .map(letra => letrasAdivinadas.includes(letra) ? letra : '_' )
+        .join('')
+      
+      if (!palabraOculta.textContent.includes('_')) {
+        console.log('Ganaste!')
+
+        botonReiniciar.classList.toggle('hidden')
+      }
+    })
+
+    botones.appendChild(boton)
   })
 
-  botones.innerHTML = contenido
+  // let contenido = ''
+
+  // ALFABETO.forEach(letra => {
+  //   contenido = contenido + `
+  //     <button class="bg-blue-500 p-2 text-white font-bold text-xl cursor-pointer hover:bg-blue-600 duration-300 rounded">${letra}</button>
+  //   `
+  // })
+
+  // botones.innerHTML = contenido
 }
+
+// TODO: Reiniciar el juego al presionar el boton '¡Empezar de nuevo!
+
+botonReiniciar.addEventListener('click', () => {
+  letrasAdivinadas = ''
+
+  palabraOculta.textContent = '_________'
+
+  botonReiniciar.classList.toggle('hidden') // Oculta nuevamente el boton reiniciar
+})
 
 renderizarAlfabeto()
