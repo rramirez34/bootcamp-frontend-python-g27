29396d53e396ebc5492f4b008e82c2ed24627040
Richard Ramírez <rramirez34@yahoo.com>
2025-10-02 19:51:08 -0500

    sem9 jueves

--- semana09/dia02-01-react-intro/src/App.jsx
+++ semana09/dia02-01-react-intro/src/App.jsx
@@ -2,8 +2,16 @@
 
 function App() {
   return (
-    <h1>Vite + React + JSX</h1>
+    <h1>Vite + React + JSX - hola</h1>
   )
 }
 
 export default App
+
+
+// 00 - ESM
+import defecto, { sumar, PI } from './javascript-esm-modules.js'
+
+console.log(sumar(43, 89))
+console.log(PI)
+console.log(defecto())
