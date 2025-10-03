2f2c05298161fcb56abb61930101f64916d69c13
Richard Ramírez <rramirez34@yahoo.com>
2025-10-02 22:33:45 -0500

    jueves 1 oct act

--- semana10/dia01-02-react-calculadora-tailwindcss/src/App.jsx
+++ semana10/dia01-02-react-calculadora-tailwindcss/src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [numeroActual, setNumeroActual] = useState('0')
+  const [operador, setOperador] = useState('')
+  const [operando, setOperando] = useState('')
+
+  const handleButton = () => {
+    console.log('Click')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Calculadora con JS</h1>
+
+      <section>
+        <input
+          readOnly
+          value={numeroActual}
+        />
+
+        <div>
+          <button onClick={handleButton}>+</button>
+          <button onClick={handleButton}>-</button>
+          <button onClick={handleButton}>*</button>
+
+          <button onClick={handleButton}>7</button>
+          <button onClick={handleButton}>8</button>
+          <button onClick={handleButton}>9</button>
+          <button onClick={handleButton}>4</button>
+          <button onClick={handleButton}>5</button>
+          <button onClick={handleButton}>6</button>
+          <button onClick={handleButton}>1</button>
+          <button onClick={handleButton}>2</button>
+          <button onClick={handleButton}>3</button>
+
+          <button onClick={handleButton}>CE</button>
+          <button onClick={handleButton}>=</button>
+        </div>
+      </section>
+    </main>
   )
 }
 
